2e780290c709458c0274945eab4a9bda755ee00e
EzeJuarez <eze-juarez@live.com>
2024-11-14 21:00:03 -0300

    search bar

--- js/index.js
+++ js/index.js
@@ -1,9 +1,11 @@
+const container = document.getElementsByClassName("container")[0];
+const boxContainer = document.getElementsByClassName("box-container")[0];
+
 fetch("https://dunamis-back.onrender.com").then(res => res.json().then(response => {
   const container2 = document.getElementsByClassName("container2")[0];
   const count = document.createElement("h3");
   count.innerHTML = `Cantidad de jóvenes: ${response.cantidad}`;
   container2.appendChild(count);
-  const boxContainer = document.getElementsByClassName("box-container")[0];
   for (joven of response.jovenes) {
     const card = document.createElement("div");
     card.classList.add("box");
@@ -24,37 +26,45 @@ fetch("https://dunamis-back.onrender.com").then(res => res.json().then(response 
     boxContainer.appendChild(card);
   };
 })).catch(error => {
-  console.log(error);
+  alert(error);
 });
 
 const btnBuscar = document.getElementById("search-button");
 const inputBuscar = document.getElementById("search-input");
+
 btnBuscar.addEventListener("click", e => {
   e.preventDefault();
   if(!inputBuscar.value) {
-    alert("No se encuentra registrado");
+    alert("Escriba un nombre");
   }else {
     fetch(`https://dunamis-back.onrender.com?nombre=${inputBuscar.value}`).then(res => res.json().then(response => {
-      inputBuscar.value = "";
-      const container = document.getElementById("searchBar-container");
-      for (joven of response) {
-        const card = document.createElement("div");
-        card.classList.add("box");
-        const name = document.createElement("p");
-        const lastname = document.createElement("p");
-        const age = document.createElement("p");
-        const phone = document.createElement("a");
-        name.innerHTML = `Nombre: ${joven.name}`;
-        lastname.innerHTML = `Apellido: ${joven.lastname}`;
-        age.innerHTML = `Edad: ${joven.age}`;
-        phone.innerHTML = "Enviar mensaje";
-        phone.href = `https://wa.me/+549${joven.phone}`;
-        phone.classList.add("btn");
-        card.appendChild(name);
-        card.appendChild(lastname);
-        card.appendChild(age);
-        card.appendChild(phone);
-        container.appendChild(card);
+      if(response.length) {
+        container.removeChild(container.children[2]);
+        const newContainer = document.createElement("div");
+        newContainer.classList.add("box-container");
+        container.appendChild(newContainer);
+        inputBuscar.value = "";
+        for (joven of response) {
+          const card = document.createElement("div");
+          card.classList.add("box");
+          const name = document.createElement("p");
+          const lastname = document.createElement("p");
+          const age = document.createElement("p");
+          const phone = document.createElement("a");
+          name.innerHTML = `Nombre: ${joven.name}`;
+          lastname.innerHTML = `Apellido: ${joven.lastname}`;
+          age.innerHTML = `Edad: ${joven.age}`;
+          phone.innerHTML = "Enviar mensaje";
+          phone.href = `https://wa.me/+549${joven.phone}`;
+          phone.classList.add("btn");
+          card.appendChild(name);
+          card.appendChild(lastname);
+          card.appendChild(age);
+          card.appendChild(phone);
+          newContainer.appendChild(card);
+        };
+      } else {
+        alert("No se encuentra registrado");
       };
     }));
   };
